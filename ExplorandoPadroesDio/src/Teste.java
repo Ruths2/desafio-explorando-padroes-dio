46c38651a69c4502b3094bb0bd35491aab342dc4
Ruths2 <ruth09@yahoo.com>
2022-03-08 06:09:42 -0300

    Cria clases SingletonLazy, SingletonEager e SingletonLazyHolder. Criar class Teste e verifica se os Singletons estão funcionando. Tudo ok

---
+++ ExplorandoPadroesDio/src/Teste.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import one.digitalinnovation.gof.SingletonLazy;
+import one.digitalinnovation.gof.SingletonEager;
+import one.digitalinnovation.gof.SingletonLazyHolder;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+      
+      SingletonLazy lazy = SingletonLazy.getInstancia();
+      System.out.println(lazy);
+      lazy = SingletonLazy.getInstancia();
+      System.out.println(lazy);
+      
+      SingletonEager eager = SingletonEager.getInstancia();
+      System.out.println(eager);
+      eager = SingletonEager.getInstancia();
+      System.out.println(eager);
+      
+      SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+      System.out.println(lazyHolder);
+      lazyHolder = SingletonLazyHolder.getInstancia();
+      System.out.println(lazyHolder);
+      
+    }
+}
